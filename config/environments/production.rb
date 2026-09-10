b3b60fe06a7f52785a1ca586ae892a8cca05c548
Rasitha Hewage <121127737+rasithahewage@users.noreply.github.com>
2026-09-10 08:05:07 +0000

    Configure production: poseidon URL, mail domain, SMTP settings, include config files needed for Docker deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,13 +101,14 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = {host: 'example.com'}
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('SMTP_ADDRESS', nil),
+    port: ENV.fetch('SMTP_PORT', 587).to_i,
+    user_name: ENV.fetch('SMTP_USERNAME', nil),
+    password: ENV.fetch('SMTP_PASSWORD', nil),
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
